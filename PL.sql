13145a7804fd8414822f25f311258723bf643c9c
Connor <alleconn@oregonstate.edu>
2026-03-16 22:18:21 -0700

    fixed typo

--- PL.sql
+++ PL.sql
@@ -174,9 +174,6 @@ DELIMITER ;
 -- Players
 
 
-hh
-
-
 -- Worlds
 
 
@@ -319,7 +316,7 @@ CREATE PROCEDURE pl_update_FarmItem(
 BEGIN
     UPDATE FarmItems
     SET item_name = itemFarmName_input, item_yield_per_hour = itemYield_input, farm_id = FarmIdFk_input
-    WHERE farm_item_id = ;
+    WHERE farm_item_id = farm_item_id_input;
 END //
 DELIMITER ;
 
